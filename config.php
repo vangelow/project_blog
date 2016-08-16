8a636682dc0051c4c266f0230898e1298516e00a
vangelow <victorangelov1@gmail.com>
2016-08-16 15:31:56 +0300

    Added the first files to the repo
    Added the first files to the repo

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('APP_ROOT', '/project_blog');
+
+define('DEFAULT_CONTROLLER', 'home');
+define('DEFAULT_ACTION', 'index');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'project_blog');
